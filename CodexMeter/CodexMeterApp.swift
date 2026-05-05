cee68de4cf805db4bdac9c055dcbbf2dec5eee60
Kristof Dewilde <code@qrivi.dev>
2026-05-05 10:24:52 +0200

    Initial Commit

---
+++ CodexMeter/CodexMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexMeterApp.swift
+//  CodexMeter
+//
+//  Created by Kristof Dewilde on 2026-05-05.
+//
+
+import SwiftUI
+
+@main
+struct CodexMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
